d68f00b293bc45961558943944b4fe47a665c755
ericyamashita <73553593+ericyamashita@users.noreply.github.com>
2020-11-20 18:01:05 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="utf-8">
+  <title>Texto Animado com efeitos</title>
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+  <body>
+    <h2 data-text="NewLogo...">NewLogo...</h2>
+  </body>
+</html>
